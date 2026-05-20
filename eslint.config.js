6c49530571365fbb6608ca3612e8022206aa1cea
Noah Gullo <noahgulloingram@gmail.com>
2026-05-19 19:26:44 -0700

    setup: basic files, HashMap, and hash function

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,5 @@
-import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: { globals: globals.browser },
-  },
+  { ignores: ["**/*.js", "**/*.cjs", "**/*.mjs"] },
 ]);
